fd114fd9b85a804533036de8d66a4a84a4e62d60
karpovVS <kazankarpov@gmail.com>
2017-08-27 22:28:20 +0300

    Update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,9 +18,11 @@ class CategoriesController < ApplicationController
     end
   end
   
-    def show
-    
+  def show
+  @category = Category.find(params[:id])
+  @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)        
   end
+
 private 
   def category_params
     params.require(:category).permit(:name)
